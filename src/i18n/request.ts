f6e337004ec616d234eb170e3a097c295249633d
Vinicius <vncsrmro@gmail.com>
2025-12-05 14:50:17 -0300

    fix: remove middleware, simplify i18n for Vercel

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,4 @@
 import { getRequestConfig } from 'next-intl/server'
-import { cookies } from 'next/headers'
 
 export const locales = ['pt-BR', 'en'] as const
 export const defaultLocale = 'pt-BR'
@@ -7,11 +6,8 @@ export const defaultLocale = 'pt-BR'
 export type Locale = (typeof locales)[number]
 
 export default getRequestConfig(async () => {
-    const cookieStore = await cookies()
-    const locale = (cookieStore.get('locale')?.value as Locale) || defaultLocale
-
     return {
-        locale,
-        messages: (await import(`@/messages/${locale}.json`)).default
+        locale: defaultLocale,
+        messages: (await import(`@/messages/${defaultLocale}.json`)).default
     }
 })
